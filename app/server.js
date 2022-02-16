7aae2729f8c95670f41e348740e76ed1e0cb947e
Rahul <srahul3@github.com>
2022-02-16 16:36:19 +0000

    Port typo correction

--- app/server.js
+++ app/server.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
    res.end( "{ page: 'Home' }");
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(8080, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
