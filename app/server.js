a6e93142536eb29b0654f2143991d1349209411c
Rahul <srahul3@github.com>
2022-02-18 10:23:04 +0000

    Allow CORS

--- app/server.js
+++ app/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var cors = require('cors')
 var app = express();
 var fs = require("fs");
+
+app.use(cors({credentials: true, origin: true}))
 
 const mongoose = require("mongoose");
 const votingCandidate = require("./model");
